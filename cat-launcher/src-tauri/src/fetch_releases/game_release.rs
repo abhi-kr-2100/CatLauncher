78ad2f8e74ceb74d5a63a1265fd538d7a4934411
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-09-28 12:41:04 +0530

    Fetch and list releases

---
+++ cat-launcher/src-tauri/src/fetch_releases/game_release.rs
@@ -0,0 +1,19 @@
+use serde::{Deserialize, Serialize};
+use ts_rs::TS;
+
+use crate::variants::GameVariant;
+
+#[derive(Debug, Clone, Deserialize, Serialize, TS)]
+pub enum ReleaseType {
+    Stable,
+    Experimental,
+}
+
+
+#[derive(Debug, Clone, Deserialize, Serialize, TS)]
+#[ts(export)]
+pub struct GameRelease {
+    pub variant: GameVariant,
+    pub version: String,
+    pub release_type: ReleaseType,
+}
