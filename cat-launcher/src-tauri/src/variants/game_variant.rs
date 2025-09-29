b7c2c45becf48a7114abd176ab727ac2d310af59
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-09-30 02:37:19 +0530

    Derive useful traits for enums

--- cat-launcher/src-tauri/src/variants/game_variant.rs
+++ cat-launcher/src-tauri/src/variants/game_variant.rs
@@ -2,7 +2,9 @@ use serde::{Deserialize, Serialize};
 use strum_macros::{EnumIter, IntoStaticStr};
 use ts_rs::TS;
 
-#[derive(Debug, EnumIter, Clone, Deserialize, Serialize, IntoStaticStr, TS)]
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter, Deserialize, Serialize, IntoStaticStr, TS,
+)]
 pub enum GameVariant {
     DarkDaysAhead,
     BrightNights,
