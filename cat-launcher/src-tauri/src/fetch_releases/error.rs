1be13af26b6199c077aa6b44eda7864901d0dbe9
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-09-30 03:06:43 +0530

    Use custom error type instead of string

--- cat-launcher/src-tauri/src/fetch_releases/error.rs
+++ cat-launcher/src-tauri/src/fetch_releases/error.rs
@@ -1,7 +1,10 @@
 use reqwest::Error as ReqwestError;
+use serde::ser::Serialize as SerializeTrait;
+use serde::ser::Serializer;
+use serde::Serialize;
 use thiserror::Error as ThisError;
 
-#[derive(Debug, ThisError)]
+#[derive(Debug, ThisError, Serialize)]
 pub enum FetchReleasesError {
     #[error("Failed to fetch GitHub releases: {0}")]
     Github(#[from] GithubFetchError),
@@ -10,8 +13,17 @@ pub enum FetchReleasesError {
 #[derive(Debug, ThisError)]
 pub enum GithubFetchError {
     #[error("Request failed: {0}")]
-    Request(#[from] ReqwestError),
+    Request(ReqwestError),
 
     #[error("Deserialization failed: {0}")]
-    Deserialize(String),
+    Deserialize(ReqwestError),
+}
+
+impl SerializeTrait for GithubFetchError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
 }
