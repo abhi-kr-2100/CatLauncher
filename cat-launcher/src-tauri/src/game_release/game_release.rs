67b093dfed1df804f501bfc9191081e40833e6cd
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-10-01 02:37:45 +0530

    Move releases and GitHub into their own modules

---
+++ cat-launcher/src-tauri/src/game_release/game_release.rs
@@ -0,0 +1,46 @@
+use std::env::consts::OS;
+
+use serde::{Deserialize, Serialize};
+use ts_rs::TS;
+
+use crate::fetch_releases::utils::get_assets;
+use crate::game_release::error::GameReleaseError;
+use crate::variants::GameVariant;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize, TS)]
+pub enum ReleaseType {
+    Stable,
+    Experimental,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, TS)]
+#[ts(export)]
+pub struct GameRelease {
+    pub variant: GameVariant,
+    pub version: String,
+    pub release_type: ReleaseType,
+}
+
+impl GameRelease {
+    pub fn get_asset_download_url(&self) -> Result<String, GameReleaseError> {
+        let assets = get_assets(self);
+
+        let asset = match (self.variant, OS) {
+            (GameVariant::DarkDaysAhead, "windows") => Some("windows-with-graphics-and-sounds"),
+            (GameVariant::DarkDaysAhead, "macos") => Some("osx-terminal-only"),
+            (GameVariant::DarkDaysAhead, "linux") => Some("linux-with-graphics-and-sounds"),
+            (GameVariant::BrightNights, "windows") => Some("windows-tiles"),
+            (GameVariant::BrightNights, "macos") => Some("osx-tiles-arm"),
+            (GameVariant::BrightNights, "linux") => Some("linux-tiles"),
+            (GameVariant::TheLastGeneration, "windows") => Some("windows-tiles-sounds-x64-msvc"),
+            (GameVariant::TheLastGeneration, "macos") => Some("osx-tiles-universal"),
+            (GameVariant::TheLastGeneration, "linux") => Some("linux-tiles-sounds"),
+            _ => None,
+        }
+        .and_then(|substring| assets.iter().find(|a| a.name.contains(substring)));
+
+        asset
+            .map(|a| a.browser_download_url.clone())
+            .ok_or(GameReleaseError::NoCompatibleAssetFound)
+    }
+}
