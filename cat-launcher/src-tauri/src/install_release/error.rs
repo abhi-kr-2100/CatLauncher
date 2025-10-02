11ac4d70b74108658e5a56b82ec6013b1de351ad
Abhishek Kumar <abhi.kr.2100@gmail.com>
2025-10-02 18:42:45 +0530

    Add ability to download releases

---
+++ cat-launcher/src-tauri/src/install_release/error.rs
@@ -0,0 +1,45 @@
+use std::io::Error as IoError;
+
+use downloader::Error as DownloaderError;
+use serde::ser::{SerializeStruct, Serializer};
+use serde::Serialize;
+use strum_macros::IntoStaticStr;
+use thiserror::Error as ThisError;
+
+use crate::game_release::error::GameReleaseError;
+use crate::infra::github::error::GitHubError;
+
+#[derive(Debug, ThisError, IntoStaticStr)]
+pub enum InstallReleaseError {
+    #[error("Failed to set up asset download directory: {0}")]
+    AssetDownloadDir(#[from] IoError),
+
+    #[error("Failed to set up downloader: {0}")]
+    Downloader(#[from] DownloaderError),
+
+    #[error("No compatible asset found for the game release")]
+    NoCompatibleAssetFound(#[from] GameReleaseError),
+
+    #[error("Failed to download asset: {0}")]
+    Download(#[from] GitHubError),
+
+    #[error("Unknown error")]
+    Unknown,
+}
+
+impl Serialize for InstallReleaseError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut st = serializer.serialize_struct("InstallReleaseError", 2)?;
+
+        let err_type: &'static str = self.into();
+        st.serialize_field("type", &err_type)?;
+
+        let msg = self.to_string();
+        st.serialize_field("message", &msg)?;
+
+        st.end()
+    }
+}
