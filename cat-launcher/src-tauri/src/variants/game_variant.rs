1ec5281345201dc3098c6756935a2967b8263e81
Abhishek Kumar <abhi.kr.2100@gmail.com>
2026-01-03 08:28:54 +0530

    refactor(settings): remove games from settings and hardcode in GameVariant

--- cat-launcher/src-tauri/src/variants/game_variant.rs
+++ cat-launcher/src-tauri/src/variants/game_variant.rs
@@ -3,7 +3,6 @@ use strum::{Display, EnumIter, EnumString, IntoStaticStr};
 use ts_rs::TS;
 
 use crate::game_release::game_release::ReleaseType;
-use crate::settings::Settings;
 use crate::variants::links::Link;
 
 #[derive(
@@ -33,20 +32,56 @@ impl GameVariant {
     self.into()
   }
 
-  pub fn name<'a>(&self, settings: &'a Settings) -> &'a str {
-    settings
-      .games
-      .get(self.id())
-      .map(|variant_settings| variant_settings.name.as_str())
-      .unwrap_or_else(|| self.id())
+  pub fn name(&self) -> &'static str {
+    match self {
+      GameVariant::DarkDaysAhead => "Dark Days Ahead",
+      GameVariant::BrightNights => "Bright Nights",
+      GameVariant::TheLastGeneration => "The Last Generation",
+    }
   }
 
-  pub fn links<'a>(&self, settings: &'a Settings) -> &'a [Link] {
-    settings
-      .games
-      .get(self.id())
-      .map(|variant_settings| variant_settings.links.as_slice())
-      .unwrap_or_else(|| &[])
+  pub fn links(&self) -> Vec<Link> {
+    match self {
+      GameVariant::DarkDaysAhead => vec![
+        Link {
+          label: "Guide".to_string(),
+          href: "https://cdda-guide.nornagon.net/".to_string(),
+        },
+        Link {
+          label: "Discord".to_string(),
+          href: "https://discord.gg/jFEc7Yp".to_string(),
+        },
+        Link {
+          label: "Reddit".to_string(),
+          href: "https://www.reddit.com/r/cataclysmdda/".to_string(),
+        },
+      ],
+      GameVariant::BrightNights => vec![
+        Link {
+          label: "Guide".to_string(),
+          href: "https://next.cbn-guide.pages.dev/".to_string(),
+        },
+        Link {
+          label: "Discord".to_string(),
+          href: "https://discord.gg/XW7XhXuZ89".to_string(),
+        },
+        Link {
+          label: "Reddit".to_string(),
+          href: "https://www.reddit.com/r/cataclysmbn/".to_string(),
+        },
+      ],
+      GameVariant::TheLastGeneration => vec![
+        Link {
+          label: "Discord".to_string(),
+          href: "https://discord.com/invite/zT9sXmZNCK".to_string(),
+        },
+        Link {
+          label: "Wiki".to_string(),
+          href: "https://cataclysmtlg.miraheze.org/wiki/Main_Page"
+            .to_string(),
+        },
+      ],
+    }
   }
 
   pub fn determine_release_type(
