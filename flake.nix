5403eb66153dec5879de3146feed7b69b7a6283d
Abhishek Kumar <abhi.kr.2100@gmail.com>
2026-05-24 19:59:07 +0530

    Squashed 'ext/github-mock-api/' content from commit 51e7536
    git-subtree-dir: ext/github-mock-api
    git-subtree-split: 51e75363e2479edd426590c2fd10d2347d83dbe4

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "GitHub Mock API devshell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      llvm = pkgs.llvmPackages.llvm;
+      libclang = pkgs.llvmPackages.libclang;
+      jna = pkgs.jna;
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          cargo-llvm-cov
+          catch2
+          check
+          clang
+          dart
+          jna
+          kotlin
+          libclang
+          llvm
+          pkg-config
+          python3
+          python3Packages.pytest
+          ruby
+          (ruby.withPackages (ps: with ps; [ rspec ]))
+          rustc
+        ];
+
+        shellHook = ''
+          export LLVM_COV="${llvm}/bin/llvm-cov"
+          export LLVM_PROFDATA="${llvm}/bin/llvm-profdata"
+          export LIBCLANG_PATH="${libclang.lib}/lib"
+          export JNA_JAR="${jna}/share/java/jna.jar"
+        '';
+      };
+    });
+}
